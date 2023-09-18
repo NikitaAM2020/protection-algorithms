f4f3f9ea4326552b25661d8a6e1a753478b972e6
NikitaAM2020 <nikita.zhuravel24@gmail.com>
2023-09-18 09:52:27 +0300

    update lab2

--- lab2/main.py
+++ lab2/main.py
@@ -1,48 +1,38 @@
-# 1. Реалізація процедури шифрування пропорційної заміни
-
 def create_substitution_table():
     # Створимо таблицю для шифру пропорційної заміни.
-    # В цьому прикладі використаємо відображення українського алфавіту в двоцифрові числа.
-    alphabet = 'АБВГҐДЕЄЖЗИІЇКЛМНОПРСТУФХЦЧШЩЬЮЯ'
-    number_mapping = {}
+    # Використаємо наведену таблицю з двоцифровими числами.
+    substitution_table = {
+        'А': '10113358', 'Б': '1174', 'В': '071228525974', 'Г': '137785', 'Ґ': '01', 'Д': '08147981',
+        'Е': '1585919299', 'Є': '081698', 'Ж': '1771', 'З': '077086', 'И': '1928354886',
+        'І': '132044', 'Ї': '212255', 'К': '02223354', 'Л': '2355', 'М': '052496',
+        'Н': '2541444569', 'О': '0317265776', 'П': '022786', 'Р': '28295770', 'С': '11212945',
+        'Т': '1027305174', 'У': '093154', 'Ф': '32', 'Х': '315963', 'Ц': '28',
+        'Ч': '213599', 'Ш': '10', 'Щ': '29', 'Ь': '05385663', 'Ю': '3943',
+        'Я': '044051', ' ': '1540415052536067757986', '.': '10122032496771' , ',': '1934566378'
+    }
+    return substitution_table
 
-    for i in range(len(alphabet)):
-        number_mapping[alphabet[i]] = str(i + 10)  # Додамо 10 для отримання двоцифрового числа
-
-    return number_mapping
-
-
-def encrypt(plaintext, number_mapping):
+def encrypt(plaintext, substitution_table):
     ciphertext = ''
 
     for char in plaintext:
-        if char.isalpha():
-            is_upper = char.isupper()
-            char = char.upper()
-
-            if char in number_mapping:
-                substitution_char = number_mapping[char]
-                if not is_upper:
-                    substitution_char = substitution_char.lower()
-                ciphertext += substitution_char
-            else:
-                ciphertext += char
+        char = char.upper()  # Конвертуємо у верхній регістр
+        if char in substitution_table:
+            ciphertext += substitution_table[char]
         else:
             ciphertext += char
 
     return ciphertext
 
-# 2. Зчитування тексту про місцевість з файлу plaintext.txt
-
+# Зчитуємо відкритий текст з файлу
 with open('plaintext.txt', 'r', encoding='utf-8') as file:
-    locality_description = file.read()
+    text = file.read()
 
-# 3. Зашифрування тексту та запис криптограми у файл
-
-number_mapping = create_substitution_table()
-ciphertext = encrypt(locality_description, number_mapping)
+# Зашифруємо текст і запишемо криптограму у файл
+substitution_table = create_substitution_table()
+ciphertext = encrypt(text, substitution_table)
 
 with open('ciphertext.txt', 'w', encoding='utf-8') as file:
     file.write(ciphertext)
 
-print('Текст успішно зашифровано та записано у файл ciphertext.txt')
+print('Текст успішно зашифровано')
